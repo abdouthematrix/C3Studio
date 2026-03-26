f60b878db89abfe7cff89b36127d0a56c81fc2e0
AbdouMatrix <AbdouMatrix@THEMATRIX>
2026-03-26 22:07:42 +0200

    Add project files.

---
+++ C3Studio/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace C3Studio
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
